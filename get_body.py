2993c6b7f05433f2591820e09abb099776574870
AyoubZarou <34717975+AyoubZarou@users.noreply.github.com>
2019-02-18 13:17:16 +0100

    Add files via upload

---
+++ get_body.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 15 23:57:01 2019
+
+@author: ZAROU AYOUB
+"""
+
+#import urllib
+import re
+url="https://support.mozilla.org/fr/products/firefox/basic-browsing-firefox" 
+import urllib.request
+html_page=urllib.request.urlopen(url).read().decode('utf-8') # we get the script from the url
+def get_body(data):
+    """
+    input : 
+    *****
+        data : a string that contains the complete html page source code
+    return :
+    ******
+        body: a parsed body, after getting rid of all the html tags, scripts,multiple line breaks or spaces.
+    
+    """
+    pattern=re.compile(r'<body(.|\n)*>(.|\n)*</body>') # Using regular expressions to find the text between the body tags
+    body=pattern.search(data) # return the data on the body
+    body=body.group() # get the result in a string
+    body=re.sub(r'<script*>*</script>','',body) # delete all the scripts 
+    body=re.sub(r'<[^>]*>','',body) # get rid of all the html tags
+    body=re.sub(r'&#39;',"'",body) # the code for ' is &#39; , for some raison it's not processed with the decode function, so we do it afterwards 
+    body=re.sub(r'\n(\s)+','\n',body) # get rid of the multispaces and multilinebreaks and use instead one linebreak
+    with open('body.txt','w') as file :  
+        file.write(body) # write the results on a text file named body.txt on the same directory as the script
+        file.close()
+    return body
+
+get_body(html_page)
+
+
+
+    
---
+++ process_body.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 12:44:55 2019
+
+@author: ZAROU AYOUB
+"""
+import re
+import spacy 
+nlp=spacy.load('fr_core_news_sm') # this might take a little time
+def process_body(body=None):
+    """
+    input : 
+    *****
+        body : a string that contains the body after being processed and structured
+    return :
+    ******
+        associations: it contains the verbs on the file and all the nouns that may go hand in hand 
+        with the verb, the result is stored in a python dictionary structure
+        
+    the result is also stored on a text file named processed_data.txt
+    
+    """    
+    if not body: 
+        try :  # if body not given , we try to read it from a text file
+            with open('body.txt','r') as f :
+                body= f.read()
+                f.close()
+        except : 
+            print('No body given, the parameter is necessary if there is no file body.txt, you may need to\
+                  execute get_body first')
+            return
+    associations={}
+    for line in body.splitlines(): # we process each line on its own
+        doc=nlp(line)
+        verb_pattern=re.compile(r'.*ée?s?$') # a pattern to spot all the adjectives that nlp happens to identify as verbs
+        verb_in_the_line=None
+        for token in doc :
+            if token.pos_=="VERB" and not verb_pattern.match(token.text):
+               verb_in_the_line=token.text
+            if verb_in_the_line and token.pos_=="NOUN":
+                if verb_in_the_line in associations:
+                    associations[verb_in_the_line].append(token.text)
+                else : 
+                    associations[verb_in_the_line]=[token.text]
+    with open('processed_data.txt','w') as f : # we store the results on a text file 
+        for key in associations : 
+            line=key +" "*(20-len(key))+':  '+', '.join(associations[key])+'\n'
+            f.write(line)
+    return associations
+process_body()
