d007bf2c0cddc9e51f5855f354e4b812d80c248e
AyoubZarou <34717975+AyoubZarou@users.noreply.github.com>
2019-12-19 17:29:03 +0100

    Update process_body.py

--- process_body.py
+++ process_body.py
@@ -7,6 +7,8 @@ Created on Mon Feb 18 12:44:55 2019
 import re
 import spacy 
 nlp=spacy.load('fr_core_news_sm') # this might take a little time
+
+
 def process_body(body=None):
     """
     input : 
@@ -18,16 +20,17 @@ def process_body(body=None):
         with the verb, the result is stored in a python dictionary structure
         
     the result is also stored on a text file named processed_data.txt
+    """   
     
-    """    
-    if not body: 
+    if body is None: 
         try :  # if body not given , we try to read it from a text file
             with open('body.txt','r') as f :
                 body= f.read()
                 f.close()
         except : 
-            print('No body given, the parameter is necessary if there is no file body.txt, you may need t execute get_body first')
-            return
+
+            raise ValueError(('No body given, the parameter is necessary if" 
+                            "there is no file body.txt, you may need t execute get_body first'))
     associations={}
     for line in body.splitlines(): # we process each line on its own
         doc=nlp(line)
@@ -46,4 +49,6 @@ def process_body(body=None):
             line=key +" "*(20-len(key))+':  '+', '.join(associations[key])+'\n'
             f.write(line)
     return associations
-process_body()
+
+if __name__ == "__main__":
+    process_body()
