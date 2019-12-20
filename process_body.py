8b375694f097f7b26da819e3e7730b3e7c1e410f
AyoubZarou <34717975+AyoubZarou@users.noreply.github.com>
2019-12-20 10:12:21 +0100

    Update process_body.py

--- process_body.py
+++ process_body.py
@@ -3,27 +3,25 @@ import spacy
 from collections import defaultdict
 
 nlp=spacy.load('fr_core_news_sm') # this might take a little time
+BODY_FILE_NAME = 'body.txt
+PROCESSED_FILE_NAME  = 'processed_data.txt'
 
 def process_body(body=None):
     """
     input : 
-    *****
-        body : a string that contains the body after being processed and structured
+        :body - a string that contains the body after being processed and structured
     return :
-    ******
-        associations: it contains the verbs on the file and all the nouns that may go hand in hand 
+        :associations - it contains the verbs on the file and all the nouns that may go hand in hand 
         with the verb, the result is stored in a python dictionary structure
-        
-    the result is also stored on a text file named processed_data.txt
+    the result is also stored on a text file( PROCESSED_FILE_NAME)
     """   
     
     if body is None: 
         try:  # if body not given , we try to read it from a text file
-            with open('body.txt', 'r') as f:
+            with open(BODY_FILE_NAME, 'r') as f:
                 body= f.read()
                 f.close()
         except: 
-
             raise ValueError(("No body given, the parameter is necessary if" 
                             "there is no file body.txt, you may need t execute get_body first"))
     associations = defaultdict(list)
@@ -36,7 +34,7 @@ def process_body(body=None):
                verb_in_the_line=token.text
             if (verb_in_the_line and token.pos_ == "NOUN"):
                 associations[verb_in_the_line].append(token.text)
-    with open('processed_data.txt', 'w') as f: # we store the results on a text file 
+    with open(PROCESSED_FILE_NAME, 'w') as f: # we store the results on a text file 
         for key in associations : 
             line=f"{key} {' '*(20-len(key))} :  {', '.join(associations[key])} \n"
             f.write(line)
