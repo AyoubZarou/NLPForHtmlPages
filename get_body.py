89d7ea405c69c652318bd0f7939762656870306c
AyoubZarou <34717975+AyoubZarou@users.noreply.github.com>
2019-12-20 10:08:02 +0100

    Update get_body.py

--- get_body.py
+++ get_body.py
@@ -1,23 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 15 23:57:01 2019
-
-@author: ZAROU AYOUB
-"""
-
-#import urllib
 import re
-import urllib.request
+import urllib.request as request
 
-
+FILE_NAME = 'body.txt' # where to save the file
 def get_body(data):
-    """
-    input : 
-    *****
-        data : a string that contains the complete html page source code
+    """ get the body from a web page
+    input:
+     :data - a string that contains the complete html page source code
     return :
-    ******
-        body: a parsed body, after getting rid of all the html tags, scripts,multiple line breaks or spaces.
+     :body - a parsed body, after getting rid of all the html tags, scripts,multiple line breaks or spaces.
     """
     pattern = re.compile(r'<body(.|\n)*>(.|\n)*</body>') # Using regular expressions to find the text between the body tags
     body = pattern.search(data) # return the data on the body
@@ -27,14 +17,14 @@ def get_body(data):
     body = re.sub(r'&#39;',"'",body) # the code for ' is &#39; , for some raison it's not processed with the decode function,
                                      # so we do it afterwards 
     body = re.sub(r'\n(\s)+','\n',body) # get rid of the multispaces and multilinebreaks and use instead one linebreak
-    with open('body.txt','w') as file:  
+    with open(FILE_NAME, 'w') as file:  
         file.write(body) # write the results on a text file named body.txt on the same directory as the script
         file.close()
     return body
 
 if __name__ == "__main__":
-    url = "https://support.mozilla.org/fr/products/firefox/basic-browsing-firefox" 
-    html_page = urllib.request.urlopen(url).read().decode('utf-8') # we get the body from the url
+    url = ("https://support.mozilla.org/fr/products/firefox/basic-browsing-firefox") 
+    html_page = request.urlopen(url).read().decode('utf-8') # we get the body from the url
     get_body(html_page)
 
 
